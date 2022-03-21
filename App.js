6495f9d845599d1da7934a109e176d0d7f220fe1
muhammadyunusuz <yunuzmuhammad1@gmail.com>
2022-03-21 16:29:30 +0500

    fix some issues

--- App.js
+++ App.js
@@ -3,29 +3,17 @@ import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import ArticlesScreen from "./screens/Articles";
 import MainStackNavigator from "./navigators/MainStackNavigator";
-import {
-  QueryClient,
-  QueryClientProvider,
-} from "react-query";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 // Create a client
 const queryClient = new QueryClient();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <QueryClientProvider client={queryClient}>
-        <MainStackNavigator />
-      </QueryClientProvider>
-    </NavigationContainer>
-  );
+	return (
+		<NavigationContainer>
+			<QueryClientProvider client={queryClient}>
+				<MainStackNavigator />
+			</QueryClientProvider>
+		</NavigationContainer>
+	);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
--- screens/ArticleDetails.jsx
+++ screens/ArticleDetails.jsx
@@ -4,75 +4,74 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { useQuery } from "react-query";
 
 export default function ArticleDetails({ route, navigator }) {
-  const { item } = route.params.data;
+	const { item } = route.params.data;
 
-  const { isLoading, error, data } = useQuery("get data", () => {
-    return fetch(`https://jsonplaceholder.typicode.com/posts/${item.id}`).then(
-      (res) => res.json()
-    );
-  });
+	const { isLoading, error, data } = useQuery("get_data", () => {
+		return fetch(
+			`https://jsonplaceholder.typicode.com/posts/${item.id}`
+		).then((res) => res.json());
+	});
 
-  return (
-    <SafeAreaView>
-      {isLoading ? (
-        <Text>Loading</Text>
-      ) : (
-        <View style={styles.container}>
-          <Text style={styles.sectionTitle}>{data.title}</Text>
-          <Image source={{ uri: item.image }} style={styles.image} />
-          <Text style={styles.category}>{item.category}</Text>
-          <View style={styles.contentWrapper}>
-            <Text style={styles.content}>{data.body}</Text>
-          </View>
-          <View style={styles.info}>
-            <Text style={styles.author}>{item.author}</Text>
-            <Text style={styles.date}>{item.date}</Text>
-          </View>
-        </View>
-      )}
-    </SafeAreaView>
-  );
+	return (
+		<>
+			{isLoading ? (
+				<Text>Loading</Text>
+			) : (
+				<View style={styles.container}>
+					<Text style={styles.sectionTitle}>{data.title}</Text>
+					<Image source={{ uri: item.image }} style={styles.image} />
+					<Text style={styles.category}>{item.category}</Text>
+					<View style={styles.contentWrapper}>
+						<Text style={styles.content}>{data.body}</Text>
+					</View>
+					<View style={styles.info}>
+						<Text style={styles.author}>{item.author}</Text>
+						<Text style={styles.date}>{item.date}</Text>
+					</View>
+				</View>
+			)}
+		</>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    padding: 16,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 16,
-    marginTop: 16,
-  },
-  card: {
-    marginTop: 5,
-    height: 300,
-  },
-  image: {
-    width: "100%",
-    height: 200,
-    borderRadius: 10,
-  },
-  category: {
-    fontSize: 12,
-    color: "blue",
-    textTransform: "uppercase",
-    marginTop: 8,
-  },
-  title: {
-    fontSize: 16,
-    fontWeight: "bold",
-  },
-  contentWrapper: {
-    marginTop: 8,
-  },
-  info: {
-    marginTop: 8,
-    fontSize: 12,
-    color: "grey",
-    flexDirection: "row",
-  },
-  author: {
-    marginRight: "auto",
-  },
+	container: {
+		padding: 16,
+	},
+	sectionTitle: {
+		fontSize: 24,
+		fontWeight: "bold",
+		marginBottom: 16,
+	},
+	card: {
+		marginTop: 5,
+		height: 300,
+	},
+	image: {
+		width: "100%",
+		height: 200,
+		borderRadius: 10,
+	},
+	category: {
+		fontSize: 12,
+		color: "blue",
+		textTransform: "uppercase",
+		marginTop: 8,
+	},
+	title: {
+		fontSize: 16,
+		fontWeight: "bold",
+	},
+	contentWrapper: {
+		marginTop: 8,
+	},
+	info: {
+		marginTop: 8,
+		fontSize: 12,
+		color: "grey",
+		flexDirection: "row",
+	},
+	author: {
+		marginRight: "auto",
+	},
 });

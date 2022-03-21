3713cb06fd7c9f002e5298c54dfc65601770545d
muhammadyunusuz <yunuzmuhammad1@gmail.com>
2022-03-21 17:16:51 +0500

    add changes

--- navigators/TabBarNavigator.js
+++ navigators/TabBarNavigator.js
@@ -2,17 +2,48 @@ import React from "react";
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import ArticlesScreen from "../screens/Articles";
 import ProfileScreen from "../screens/ProfileScreen";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 export default function TabBarNavigator() {
 	const BottomNavigator = createMaterialBottomTabNavigator();
 
 	return (
-		<BottomNavigator.Navigator>
+		<BottomNavigator.Navigator
+			barStyle={{
+				backgroundColor: "red",
+				paddingTop: 5,
+			}}
+		>
 			<BottomNavigator.Screen
 				name="Articles"
 				component={ArticlesScreen}
+				options={{
+					tabBarIcon: ({ focused, color }) => {
+						return (
+							<MaterialCommunityIcons
+								name="library"
+								size={20}
+								color={color}
+							/>
+						);
+					},
+				}}
 			/>
-			<BottomNavigator.Screen name="Profile" component={ProfileScreen} />
+			<BottomNavigator.Screen
+				name="Profile"
+				component={ProfileScreen}
+				options={{
+					tabBarIcon: ({ focused, color }) => {
+						return (
+							<MaterialCommunityIcons
+								name="account"
+								size={20}
+								color={color}
+							/>
+						);
+					},
+				}}
+			/>
 		</BottomNavigator.Navigator>
 	);
 }

b31bf1c887f4778b3ed54b0a9dc0902b02bfca7a
sabohat <sabohat2802@gmail.com>
2022-04-01 19:53:52 +0500

    feat: add calendar

--- screens/ProfileScreen.jsx
+++ screens/ProfileScreen.jsx
@@ -1,19 +1,45 @@
 import { View, Text, Pressable, StyleSheet } from "react-native";
 import React from "react";
-
+import { Calendar } from "react-native-calendars";
 
 export default function ProfileScreen() {
-	return (
-		<View style={styles.container}>
-			
-		</View>
-	);
+  const vacation = { key: "vacation", color: "red", selectedDotColor: "blue" };
+  const massage = { key: "massage", color: "blue", selectedDotColor: "blue" };
+  const workout = { key: "workout", color: "green" };
+  return (
+    <View style={styles.container}>
+      <Calendar
+        style={{
+          width: "100%",
+          height: 350,
+        }}
+        markingType={"period"}
+        markedDates={{
+          "2022-04-22": {
+            startingDay: true,
+            color: "pink",
+            textColor: "white",
+          },
+          "2022-04-23": {
+            color: "pink",
+            textColor: "white",
+          },
+          "2022-04-24": { selected: true, color: "pink", textColor: "white" },
+          "2022-04-25": { selected: true, color: "pink", textColor: "white" },
+          "2022-04-26": { selected: true, color: "pink", textColor: "white" },
+          "2022-04-27": { endingDay: true, color: "pink", textColor: "white" },
+          "2022-04-16": { selected: true, marked: true, selectedColor: "blue" },
+          "2022-04-17": { marked: true },
+          "2022-04-18": { marked: true, dotColor: "red", activeOpacity: 0 },
+          "2022-04-19": { disabled: true, disableTouchEvent: true },
+        }}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: "center",
-		alignItems: "center",
-	},
+  container: {
+    flex: 1,
+  },
 });
